16d7c948106f6980ba67653051027e551468e790
John4850 <johnnelson4850@gmail.com>
2023-03-23 21:43:52 -0700

    initialize with default values

--- Models/Note.cs
+++ Models/Note.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace reactNotes.Models
 {
-    public class Note
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
+  public class Note
+  {
+    public int Id { get; set; }
+    public string Title { get; set; } = "New Note";
+    public string Description { get; set; } = "content";
+  }
 }
